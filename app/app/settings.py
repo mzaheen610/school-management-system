4303b2c7136dbcdbcb4b3b2eac427f751df821fb
Mohammed Zaheen <mohammed.zaheen@dbizsolution.com>
2024-01-30 12:25:19 +0530

    Created student app for the student API.

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
-    'user'
+    'user',
+    'student',
 ]
 
 MIDDLEWARE = [
