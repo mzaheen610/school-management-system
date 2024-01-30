39ec6a7de0053595b93268a1962296a12b6fa855
Mohammed Zaheen <mohammed.zaheen@dbizsolution.com>
2024-01-30 18:47:16 +0530

    Added serializer for student API.

--- app/student/views.py
+++ app/student/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+"""
+Views for the student API.
+"""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from student.serializers import StudentSerializer
+from core.models import Student
+
+class StudentAPIView(viewsets.ModelViewSet):
+    """Viewset for viewing and managing student profiles."""
+
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve student profile for authenticated user """
